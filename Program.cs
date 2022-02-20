66211cba00f849336d05f10dd0edd67fcfd6c46e
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-21 00:52:52 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace alistirme_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayiyi giriniz: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a<100 && a>0)
+            {
+                Console.WriteLine("Girdiginiz sayi 0-100 arasindadir");
+            }
+            else 
+            {
+                Console.WriteLine("Girdiniz sayi 0-100 arasinda degildir");
+            }
+        }
+    }
+}
